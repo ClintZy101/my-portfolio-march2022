ef633d09529a308536dfdfa4ffdb55fc7bc3d87e
ClintZy101 <clielizek.joy@gmail.com>
2022-03-18 05:06:12 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    // colors: {
+    //   'background': '#203239',
+    //   'text':'E0DDAA',
+    //   'light':'EEEDDE',
+    //   'dark':'141E27'
+    // },
+    extend: {
+    },
+  },
+  plugins: [],
+}
